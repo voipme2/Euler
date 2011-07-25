92ef6b913c3e731c186b29863776e6b3ee320c77
Pat Laughlin <voipme2@gmail.com>
2011-07-24 21:39:41 -0400

    Updated the formatting.

--- src/q1.py
+++ src/q1.py
@@ -6,8 +6,6 @@ def q1():
     of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
     
     Find the sum of all the multiples of 3 or 5 below 1000.
-
-    TODO: convert this to Python 3.x syntax
     """
     total = 0
     for i in range(1000):
