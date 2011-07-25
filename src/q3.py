e4c14220add56689d6574746b39604e9f8eb0b04
Pat Laughlin <voipme2@gmail.com>
2011-07-24 21:51:42 -0400

    Reformatted q1, and added in the framework for q2 and q3.

---
+++ src/q3.py
@@ -0,0 +1,11 @@
+'''
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143?
+'''
+
+def q3():
+    pass
+
+if __name__ == "__main__":
+    q3()
