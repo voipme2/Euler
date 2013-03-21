3e9384cf9875a6d8e039454d1e74dd9183cb7a28
Pat Laughlin <voipme2@gmail.com>
2013-03-20 20:48:48 -0400

    solution for question 25

---
+++ src/q25.py
@@ -0,0 +1,17 @@
+﻿def fibonacci():
+  a, b = 0, 1
+  while True:
+    yield a
+    a, b = b, a + b
+
+if __name__ == "__main__":
+  gen = fibonacci()
+  max_len = 0
+  count = 0
+  s = ""
+  while len(s) < 1000:
+    s = str(gen.next())
+    if len(s) > max_len:
+      max_len = len(s)
+      print "F(%d) : %d" % (count, max_len)
+    count = count + 1
