aedcf3c8ef9ff5c3ef0e337f56290a0a6c6ad69a
Pat Laughlin <voipme2@gmail.com>
2013-03-20 20:52:35 -0400

    Solution to question 18

--- src/q18.py
+++ src/q18.py
@@ -1,31 +1,25 @@
-#!/usr/bin/env python
+﻿import sys
+"""
+  Run:  q18.py files/18-triangle.txt
+"""
+def max_tri(triangle):
+  triangle.reverse()
+  for r_i in range(len(triangle) - 1):
+    row = triangle[r_i]
+    next_row = triangle[r_i + 1]
+    for i in range(len(row) - 1):
+      next_row[i] += max(row[i], row[i+1])
+  return triangle[-1][0]
 
-import sys
-
-def max_path(triangle):
-
-	index = 0
-	total = 0
-	for row in triangle:
-		print row
-		if len(row) == 1:
-			amt = row[0]
-		else:
-			l,r = index, (index + 1)
-			if index < 0:
-				l = index - 1
-			if r > len(row):
-				r = len(row)
-			amt = max(int(row[l]), int(row[r]))
-			index = row.index(amt)
-		total += amt
-	return total
+def parse_triangle(f):
+  tri = []
+  for l in f:
+    tri.append([int(n) for n in l.strip().split(" ") if len(n) > 0])
+  return tri
 
 if __name__ == "__main__":
-	if len(sys.argv) < 2:
-		print "Need a file"
-	else:
-		triangle = []
-		for l in open(sys.argv[1]):
-			triangle.append(map(lambda x: int(x), l.split(" ")))
-		print "Maximal path value %d" % max_path(triangle)
+  if len(sys.argv) < 2:
+    print "Need a triangle file."
+  else:
+    tri = parse_triangle(open(sys.argv[1]))
+    print "Max: %d" % (max_tri(tri))
